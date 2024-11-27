2cf6ff4b233f2a5b22d9ef2721fe6d0e725ef570
Christoffer Lindahl <nilschristofferlindahl@gmail.com>
2024-11-27 09:35:45 +0100

    fix: remove unused 'test-today.js' script file

--- src/scripts/test-today.js
+++
@@ -1,8 +0,0 @@
-import { exec } from "child_process";
-
-const thisYear = new Date().getFullYear();
-const day = new Date().getDate();
-
-const pattern = `${thisYear}/day${day.toString().padStart(2, "0")}`;
-
-exec(`jest --watchAll --testPathPattern=${pattern}`);
