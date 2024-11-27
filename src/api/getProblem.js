db50d80e2ad4776c31e8d0ede5d5ce4b17540f36
Christoffer Lindahl <nilschristofferlindahl@gmail.com>
2024-11-27 21:18:31 +0100

    fix: add styling to problem.html

--- src/api/getProblem.js
+++ src/api/getProblem.js
@@ -15,7 +15,13 @@ export const getProblem = async (year, day) => {
 
     const $main = new JSDOM(body).window.document.querySelector("main");
 
-    return $main?.outerHTML;
+    $main.style =
+      "background-color: #0f0f23; color: lightgray; padding: 20px; margin: -20px; font-size: 12px; font-family: monospace";
+
+    const style =
+      '<style> code { background-color: #10101a; display: inline-block; border: "1px solid grey";} em { color: white; text-shadow: white 0 0 5px;}</style>';
+
+    return `${style}${$main?.outerHTML}`;
   } catch (e) {
     return handleErrors(e);
   }
