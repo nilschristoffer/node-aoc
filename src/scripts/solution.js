87524793bff456a000515cf42bcae4ac647d8708
Christoffer Lindahl <nilschristofferlindahl@gmail.com>
2024-11-27 21:19:09 +0100

    fix: add year option to solution script

--- src/scripts/solution.js
+++ src/scripts/solution.js
@@ -1,12 +1,25 @@
 import promptSync from "prompt-sync";
 import { postSolution } from "../api/postSolution.js";
+import { colog } from "../api/helpers/helpers.js";
 
-const [fDay, fPart, fAns] = process.argv.slice(2);
+const [fYear, fDay, fPart, fAns] = process.argv.slice(2);
 
 const prompt = promptSync({ sigint: true });
 
-const day = fDay ?? prompt("Which day: ");
-const part = fPart ?? prompt("Which part: ");
-const answer = fAns ?? prompt("Your answer: ");
+const year = (fYear ?? prompt("Year: ")) || new Date().getFullYear();
+colog.default(year);
 
-postSolution(new Date().getFullYear(), day, part, answer);
+const day = (fDay ?? prompt("Day: ")) || new Date().getDate().toString();
+colog.default(day);
+
+const part = (fPart ?? prompt("Part: ")) || "1";
+colog.default(part);
+
+const answer = fAns ?? prompt("Answer: ");
+colog.default(answer);
+
+if (answer) {
+  postSolution(year, day, part, answer);
+} else {
+  colog.warn("No answer provided, exiting...");
+}
